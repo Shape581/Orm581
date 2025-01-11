4381670adb6b92dd1c225860c43218c9ffaaab08
Shape581 <severusrogue5008@gmail.com>
2025-01-11 04:32:57 +0100

    CODE

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using Life;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orm581
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[Orm581] - Succes : Intiialise");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
